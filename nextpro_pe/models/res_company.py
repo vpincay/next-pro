84b0da648378519e000ba8a6557fb01233ba04e9
vpincay <roysxp@gmail.com>
2019-09-20 00:38:48 -0300

    no message

--- nextpro_pe/models/res_company.py
+++ nextpro_pe/models/res_company.py
@@ -5,8 +5,8 @@ from odoo import fields, models, api
 class ResCompany(models.Model):
     _inherit = 'res.company'
 
-    razon_social = fields.Char(string="Razon social", required=True)
-    ruc = fields.Char(string="R.U.C.", required=True)
+    razon_social = fields.Char(string="Razon social")
+    ruc = fields.Char(string="R.U.C.")
     agente_retencion = fields.Boolean(string="Agente de retencion")
     buen_contribuyente = fields.Boolean(string="Buen contribuyente")
     plan_contable = fields.Selection(
