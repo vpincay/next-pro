d692bb548404bac10bb6874a7544c8754b4a891f
vpincay <roysxp@gmail.com>
2019-09-20 00:09:53 -0300

    no message

--- nextpro_pe/__manifest__.py
+++ nextpro_pe/__manifest__.py
@@ -21,8 +21,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'views/res_company_view.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
--- nextpro_pe/models/res_company.py
+++ nextpro_pe/models/res_company.py
@@ -5,6 +5,7 @@ from odoo import fields, models, api
 class ResCompany(models.Model):
     _inherit = 'res.company'
 
+    razon_social = fields.Char(string="Razon social", required=True)
     ruc = fields.Char(string="R.U.C.", required=True)
     agente_retencion = fields.Boolean(string="Agente de retencion")
     buen_contribuyente = fields.Boolean(string="Buen contribuyente")
